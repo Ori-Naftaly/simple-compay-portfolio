f926688dc49bbd7207bf32265bda09b8b5a6518a
Ori Naftaly <naftalyblues@gmail.com>
2020-06-29 22:50:49 -0500

    Updates

---
+++ precache-manifest.be39c7a3e67a9279552595d2058ec2f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe775e7adbf9df00fa9ad771a167ae6",
+    "url": "/simple-compay-portfolio/index.html"
+  },
+  {
+    "revision": "f8c5d9a8086cd3c8cd2e",
+    "url": "/simple-compay-portfolio/static/css/main.d5e436b9.chunk.css"
+  },
+  {
+    "revision": "c2e5acd1509d55766973",
+    "url": "/simple-compay-portfolio/static/js/2.27d7b6bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-compay-portfolio/static/js/2.27d7b6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c5d9a8086cd3c8cd2e",
+    "url": "/simple-compay-portfolio/static/js/main.209e9ff1.chunk.js"
+  },
+  {
+    "revision": "924286ba1b3ec2790da7",
+    "url": "/simple-compay-portfolio/static/js/runtime-main.dd4333a5.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/simple-compay-portfolio/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/simple-compay-portfolio/static/media/map-image.f40ed5b1.png"
+  }
+]);
